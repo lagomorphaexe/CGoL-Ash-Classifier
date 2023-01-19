d825f9d25254e934f618dd389da3f155c584d538
lagomorphaexe <68913498+lagomorphaexe@users.noreply.github.com>
2023-01-19 08:29:54 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,24 +9,27 @@ then
   echo N_THIS updated to $N_THIS.
 fi
 
-sed -i "/#____#$/s/=.*#/=$N_THIS;#____#/" ${0}
-
 GOL_RNG_FILE="search-files/unprocessed.rle"
 GOL_ADV_FILE="search-files/COUNT_${N_THIS}.rle"
 
 echo "-------------------------GENERATING RANDOM PATTERN FILE-------------------------"
 TIME_1=$(date +%s)
-./genGrid $GOL_RNG_FILE 30000 20000             # <1 min
+./genGrid $GOL_RNG_FILE 30000 20000
 TIME_2=$(date +%s)
 echo "(->$GOL_RNG_FILE), runtime: $(($TIME_2 - $TIME_1))s"
 echo "-------------------------ADVANCING RANDOM PATTERN FILE-------------------------"
 TIME_1=$(date +%s)
-bgolly -m 100000 -o $GOL_ADV_FILE $GOL_RNG_FILE  # ~4 mins
+bgolly -m 100000 -o $GOL_ADV_FILE $GOL_RNG_FILE
 TIME_2=$(date +%s)
 echo ", runtime: $(($TIME_2 - $TIME_1))s"
 echo "-------------------------ANALYZING ADVANCED FILE-------------------------"
 TIME_1=$(date +%s)
-./analyzeRLE -c -d $GOL_ADV_FILE                # ~2 mins
+./analyzeRLE -c -d $GOL_ADV_FILE
 TIME_2=$(date +%s)
 echo "runtime: $(($TIME_2 - $TIME_1))s"
 echo "-------------------------MAIN SCRIPT FINISHED----------------------------"
+
+sed -i "/#____#$/s/=.*#/=$N_THIS;#____#/" ${0}
+
+notify-send "Analysis Complete at $GOL_ADV_FILE"
+mpg123 -q ball.mp3
